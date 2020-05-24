5dfcf1f02194c7129e5b67ecaf320c371a42e966
Ivan Petkov <ipetkow90@gmail.com>
2020-05-24 15:41:03 +0300

    Adding tests for Selectable and Sortable functionality

---
+++ 03. Selenium Advanced/SeleniumAdvancedHomework/Task02. Interaction Tests/SelectableTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Linq;
+
+namespace SeleniumAdvancedHomework.Task02._Interaction_Tests
+{
+    class SelectableTests : BaseTest
+    {
+        private const string Selectable = "selectable";
+
+        private readonly By _selectableOptions = By.CssSelector("#verticalListContainer li");
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            NavigateToInteractionSection(Selectable);
+        }
+        [Test]
+        public void Test1_Verify_All_SelectableElements_Are_Selected()
+        {
+            int selectableOptionsCount = FindElements(_selectableOptions).Count;
+            for (int i = 1; i <= selectableOptionsCount; i++)
+            {
+                _driver.FindElement(By.CssSelector($"#verticalListContainer li:nth-of-type({i})")).Click();
+            }
+
+            int actualCountOfSelectedItems = FindElements(_selectableOptions)
+                .Where(x => x.GetAttribute("class").Contains("active"))
+                .Count();
+
+            int expectedCountOfSelectedItems = 4;
+            Assert.AreEqual(expectedCountOfSelectedItems, actualCountOfSelectedItems);
+        }
+
+        [Test]
+        public void Test2_Verify_AllItems_AreNot_Selected_When_They_Were_Selected()
+        {
+            Test1_Verify_All_SelectableElements_Are_Selected();
+
+            int selectableOptionsCount = FindElements(_selectableOptions).Count;
+            for (int i = 1; i <= selectableOptionsCount; i++)
+            {
+                _driver.FindElement(By.CssSelector($"#verticalListContainer li:nth-of-type({i})")).Click();
+            }
+
+            int actualCountOfNotSelectedItems = FindElements(_selectableOptions)
+                .Where(x => x.GetAttribute("class").Contains("active"))
+                .Count();
+
+            int expectedCountOfNotSelectedItems = 0;
+            Assert.AreEqual(expectedCountOfNotSelectedItems, actualCountOfNotSelectedItems);
+
+        }
+    }
+}
---
+++ 03. Selenium Advanced/SeleniumAdvancedHomework/Task02. Interaction Tests/SortableTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SeleniumAdvancedHomework.Task02._Interaction_Tests
+{
+    class SortableTests : BaseTest
+    {
+        private const string Sortable = "sortable";
+
+        private readonly By _oneBox = By.CssSelector("div.list-group-item:nth-of-type(1)");
+        private readonly By _allBoxes = By.CssSelector(".vertical-list-container div");    
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            NavigateToInteractionSection(Sortable);
+        }
+
+        [Test]
+        public void Test1_Verify_FirstBox_Is_Placed_In_The_End()
+        {
+            PerformActions()
+                .DragAndDropToOffset(FindElement(_oneBox), 0, 500)
+                .Perform();
+            List<string> expectedOrder = new List<string>()
+            {
+                "Two", "Three", "Four", "Five", "Six", "One"
+            };
+            List<string> actualOrder = new List<string>();
+
+            IList<IWebElement> allBoxes = FindElements(_allBoxes);
+            foreach (var element in allBoxes)
+            {
+                actualOrder.Add(element.Text);
+            }
+            CollectionAssert.AreEqual(expectedOrder, actualOrder);
+        }
+
+        [Test]
+        public void Test2_Verify_Order_Of_Boxes_After_Moving_Elements_And_Page_Refresh()
+        {
+            List<string> orderInBeginning = FindElements(_allBoxes)
+                .Select(x => x.Text)
+                .ToList();
+
+            for (int i = 1; i <= 6; i++)
+            {
+                PerformActions()
+                    .DragAndDropToOffset(_driver.FindElement(By.CssSelector($"div.list-group-item:nth-of-type({i})")), 0, 150)
+                    .Perform();
+            }
+            List<string> orderAfterActions = FindElements(_allBoxes).Select(x => x.Text).ToList();
+            CollectionAssert.AreNotEqual(orderInBeginning, orderAfterActions);
+
+            RefreshPage();
+
+            List<string> orderAfterResfresh = FindElements(_allBoxes).Select(x => x.Text).ToList();
+            CollectionAssert.AreEqual(orderInBeginning, orderAfterResfresh);
+        }
+    }
+}
