132c11484cb5271fa947fc21938fb361bc4c419c
Ivan Petkov <ipetkow90@gmail.com>
2020-06-08 20:56:07 +0300

    Adding tests for Sortable functionality

---
+++ 04. Best Practices - Paje Object Model/PajeObjectModel Homework/Pages/DemoQA/SortablePage.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PajeObjectModel_Homework.Pages.DemoQA
+{
+    public class SortablePage : BasePage
+    {
+        private IWebElement Box(int boxNumber) => FindElement(By.CssSelector($"div.list-group-item:nth-of-type({boxNumber})"));
+        private readonly By _allBoxes = By.CssSelector(".vertical-list-container div");
+
+        public SortablePage(IWebDriver driver, string subMenu) : base(driver)
+        {
+            GoToSpecificSubMenu(subMenu);
+        }
+
+        public void ChangePositionOfBox(int boxNumber, int offset)
+        {
+            PerformActions()
+                .DragAndDropToOffset(Box(boxNumber), 0, offset)
+                .Perform();
+        }
+
+        public List<string> OrderOfBoxes 
+        {
+            get
+            {
+                return FindElements(_allBoxes).Select(x => x.Text).ToList();
+            } 
+        }
+
+        public void ChangeOrderOfAllBoxes()
+        {
+            for (int i = 1; i <= 6; i++)
+            {
+                PerformActions()
+                    .DragAndDropToOffset(Box(i), 0, 150)
+                    .Perform();
+            }
+        }
+    }
+}
---
+++ 04. Best Practices - Paje Object Model/PajeObjectModel Homework/Tests/InteractionDemoQA/SortableTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using PajeObjectModel_Homework.Pages.DemoQA;
+using System.Collections.Generic;
+
+namespace PajeObjectModel_Homework.Tests.InteractionDemoQA
+{
+    class SortableTests : BaseTest
+    {
+        private InteractionsPage _interactionsPage;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            StartBrowser();
+            NavigateToURL(URLs.DemoQASite);
+            _interactionsPage = new HomePage(Driver).GoToInteractionsMenu();
+        }
+
+        [Test]
+        public void Test1_Verify_FirstBox_Is_Placed_In_The_End()
+        {
+            List<string> expectedOrder = new List<string>()
+            {
+                "Two", "Three", "Four", "Five", "Six", "One"
+            };
+            SortablePage sortablePage = _interactionsPage.NavigateToInteractionsSection("Sortable", "List");
+
+            sortablePage.ChangePositionOfBox(1, 500);
+
+            List<string> actualOrder = sortablePage.OrderOfBoxes;
+
+            CollectionAssert.AreEqual(expectedOrder, actualOrder);
+        }
+
+        [Test]
+        public void Test2_Verify_Order_Of_Boxes_After_Moving_Elements_And_Page_Refresh()
+        {
+            SortablePage sortablePage = _interactionsPage.NavigateToInteractionsSection("Sortable", "List");
+            List<string> orderInBeginning = sortablePage.OrderOfBoxes;
+
+            sortablePage.ChangeOrderOfAllBoxes();
+
+            List<string> orderAfterActions = sortablePage.OrderOfBoxes;
+            CollectionAssert.AreNotEqual(orderInBeginning, orderAfterActions);
+
+            sortablePage.RefreshPage();
+
+            List<string> orderAfterResfresh = sortablePage.OrderOfBoxes;
+            CollectionAssert.AreEqual(orderInBeginning, orderAfterResfresh);
+        }
+    }
+}
