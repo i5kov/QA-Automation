cdcb603ef5ef3c32ec109399e90e5e84f90c84e5
Ivan Petkov <ipetkow90@gmail.com>
2020-06-08 22:59:59 +0300

    Adding tests for Selectable functionality

---
+++ 04. Best Practices - Paje Object Model/PajeObjectModel Homework/Pages/DemoQA/SelectablePage.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using System.Linq;
+
+namespace PajeObjectModel_Homework.Pages.DemoQA
+{
+    public class SelectablePage : BasePage
+    {
+
+        private readonly By _selectableOptions = By.CssSelector("#verticalListContainer li");
+        private IWebElement SelectableOption(int option) => FindElement(By.CssSelector($"#verticalListContainer li:nth-of-type({option})"));
+
+        public SelectablePage(IWebDriver driver, string subMenu) : base(driver)
+        {
+            GoToSpecificSubMenu(subMenu);
+        }
+
+        public int SelectableOptions 
+        {
+            get
+            {
+                return FindElements(_selectableOptions).Count;
+            }
+        }
+
+        public int SelectedOptions 
+        {  
+            get
+            {
+                return FindElements(_selectableOptions)
+                    .Where(x => x.GetAttribute("class")
+                    .Contains("active"))
+                    .Count();
+            }
+        }
+
+        public void SelectOptions(int optionsCount)
+        {
+            for (int i = 1; i <= optionsCount; i++)
+            {
+                ClickElement(SelectableOption(i));
+            }
+        }
+    }
+}
---
+++ 04. Best Practices - Paje Object Model/PajeObjectModel Homework/Tests/InteractionDemoQA/SelectableTests.cs
@@ -0,0 +1,41 @@
+﻿
+
+using NUnit.Framework;
+using PajeObjectModel_Homework.Pages.DemoQA;
+
+namespace PajeObjectModel_Homework.Tests.InteractionDemoQA
+{
+    class SelectableTests : BaseTest
+    {
+
+        private InteractionsPage _interactionsPage;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            StartBrowser();
+            NavigateToURL(URLs.DemoQASite);
+            _interactionsPage = new HomePage(Driver).GoToInteractionsMenu();
+        }
+
+        [Test]
+        public void Test1_Verify_All_SelectableElements_Are_Selected()
+        {
+            SelectablePage selectablePage = _interactionsPage.NavigateToInteractionsSection("Selectable", "List");
+            int selectableOptionsCount = selectablePage.SelectableOptions;
+
+            selectablePage.SelectOptions(selectableOptionsCount);
+
+            int actualCountOfSelectedItems = selectablePage.SelectedOptions;
+
+            int expectedCountOfSelectedItems = 4;
+            Assert.AreEqual(expectedCountOfSelectedItems, actualCountOfSelectedItems);
+        }
+
+        [TearDown]
+        public void AfterEachTest()
+        {
+            CloseBrowser();
+        }
+    }
+}
